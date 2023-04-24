7cc4244931fcb7e44d4662ad3aa517ff05705aa2
Arkein Singh <60797452+singark21@users.noreply.github.com>
2023-04-23 22:06:53 -0600

    Started a list that has all flag pngs.

--- flagConstants.h
+++ flagConstants.h
@@ -19,4 +19,12 @@ const QVector<QString> UNION_JACK_FLAGS = {"au.png", "nz.png", "tv.png", "fj.png
 const QVector<QString> CORNER_FLAG_TEMPLATES = {"circleMiddleBottom.png", "circleMiddleTop.png", "flowerMiddleRight.png", "pentagonBottomLeft.png", "pentagonBottomRight.png", "treeMiddleLeft.png", "treeMiddleRight.png", "triangleTopLeft.png", "triangleTopRight.png"};
 const QVector<QString> CENTER_FLAG_TEMPLATES = {"circleMiddle.png", "flowerMiddle.png", "polygonMiddle.png", "tigerMiddle.png", "triangleMiddle.png", "weirdShapeMiddle.png"};
 
+
+const QVector<QString> ALL_FLAGS = {"ae.png","af.png","ag.png","ai.png","al.png","am.png","ao.png","aq.png","ar.png","as.png","at.png","au.png","aw.png","ax.png","az.png"
+
+
+
+
+
+                                   };
 #endif // FLAGCONSTANTS_H
